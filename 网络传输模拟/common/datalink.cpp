e581390f8384393df784d5295639862f329e3412
Kagaya <kagaya85@foxmail.com>
2018-12-04 23:48:09 +0800

    timer

--- 网络传输模拟/common/datalink.cpp
+++ 网络传输模拟/common/datalink.cpp
@@ -1,40 +1,130 @@
 #include "common.h"
+#include "Datalink.h"
 
-void enable_network_layer()
+Datalink::Datalink()
+{
+    header = NULL;
+    ackHeader = NULL;
+}
+
+Datalink::~Datalink()
+{
+    TimerNode *p;
+    while (header)
+    {
+        p = header->next;
+        delete header;
+        header = p;
+    }
+    while (ackHeader)
+    {
+        p = ackHeader->next;
+        delete ackHeader;
+        ackHeader = p;
+    }
+}
+
+void Datalink::start_timer(seq_nr k)
+{
+    TimerNode *p;
+    clock_t t;
+    if (!header)
+    {
+        header = new(nothrow) TimerNode;
+        if (header == NULL)
+        {
+            cerr << "new timerNode error" << endl;
+            exit(EXIT_FAILURE);
+        }
+        header->next = NULL;
+        header->nowTime = TIMEOUT_LIMIT;
+        header->seq = k;
+    }
+    else
+    {
+        p = header;
+        t = TIMEOUT_LIMIT
+
+        while (p->next)
+        {
+            t -= p->nowTime;
+            p = p->next;
+        }
+
+        t -= p->nowTime;
+        p->next = new(nothrow) TimerNode;
+        if (p == NULL)
+        {
+            cerr << "new timerNode error" << endl;
+            exit(EXIT_FAILURE);
+        }
+        p = p->next;
+        p->next = NULL;
+        p->nowTime = t;
+        p->seq = k;
+    }
+}
+
+void Datalink::stop_timer(seq_nr k)
+{
+    TimerNode *p, *q;
+    if (!header)
+        return;
+
+    if (header->seq == k)
+    {
+        p = header;
+        header = header->next;
+        delete header;
+        return;
+    }
+
+    p = header;
+    q = header->next;
+    while (q)
+    {
+        if(q->seq == k)
+        {
+            p->next = q->next;
+            delete q;
+            q = p->next;
+            return;
+        }
+    }    
+}
+
+void Datalink::enable_network_layer()
 {
     pid_t pid;
     pid = getPidByName("network");
     kill(pid, SIG_NETWORK_LAYER_ENABLE);
 }
 
-void disable_network_layer()
+void Datalink::disable_network_layer()
 {
     pid_t pid;
     pid = getPidByName("network");
     kill(pid, SIG_NETWORK_LAYER_DISABLE);
 }
 
-void from_network_layer(packet *pkt)
+void Datalink::from_network_layer(packet *pkt)
 {
     
 }
 
-void start_timer(seq_nr k)
+void Datalink::start_ack_timer()
 {
 
 }
 
-void stop_timer(seq_nr k)
+void Datalink::stop_ack_timer()
 {
 
 }
 
-void start_ack_timer()
+void Datalink::addSecond(int signal)
 {
 
-}
-
-void stop_ack_timer()
-{
-
+	alarm(1);
+	return;
 }
--- 网络传输模拟/common/datalink.h
+++ 网络传输模拟/common/datalink.h
@@ -1,4 +1,34 @@
 #ifndef DATALINK
 #define DATALINK
 
+#define TIMEOUT_LIMIT 10000 // 单位ms 10s 
+#include <time.h>
+#include "common.h"
+
+struct TimerNode {
+    clock_t nowTime,
+    seq_nr seq,
+    TimerNode *next
+};
+
+class Datalink {
+private:
+    TimerNode *header;
+    TimerNode *ackHeader;
+public:
+    Datalink();
+    ~Datalink();
+    void start_timer(seq_nr k);
+    void stop_timer(seq_nr k);
+    void enable_network_layer();
+    void disable_network_layer();
+    void from_network_layer(packet *pkt);
+    void start_ack_timer();
+    void stop_ack_timer();
+    // signal
+    void add_a_second(int signal);
+    void timeout_handle(int signal);
+    void ack_timeout_handle(int signal);
+};
+
 #endif // DATALINK
