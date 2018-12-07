0406d5389720adfc0a3077d1ca12c7fe0f8f2ffb
Kagaya <kagaya85@foxmail.com>
2018-12-08 01:24:09 +0800

    update protocol 4/5

--- 网络传输模拟/05/protocol5_datalink.cpp
+++ 网络传输模拟/05/protocol5_datalink.cpp
@@ -1,16 +1,48 @@
 #include "../common/datalink.h"
 
+#define MAX_SEQ 1 // 窗口大小
+
 int main()
 {
     Datalink dl;
-    frame r;
+    seq_nr next_frame_to_send;
+    seq_nr frame_expected;
+    frame r, s;
     event_type event;
+
+    next_frame_to_send = 0;
+    frame_expected = 0;
+    from_network_layer(&buffer);
+    s.info = buffer;
+    s.seq = next_frame_to_send;
+    s.ack = 1 - frame_expected;
+    dl.to_physical_layer(&s);
+    dl.start_timer(s.seq);
 
     while(true)
     {
         wait_for_event(&event);
-        dl.from_physical_layer(&r);
-        dl.to_network_layer(&r.info);
+        if (event == frame_arrival)
+        {
+            dl.from_physical_layer(&r);
+            if (r.seq == frame_expected)
+            {
+                to_network_layer(&r.info);
+                inc(frame_expected);
+            }
+            if (r.ack == next_frame_to_send)
+            {
+                dl.stop_timer(r.ack);
+                from_network_layer(&buffre);
+                inc(next_frame_to_send);
+            }
+        }
+        // 发送新包
+        s.info = buffer;
+        s.seq = next_frame_to_send;
+        s.ack = 1 - frame_expected;
+        dl.to_physical_layer(&s);
+        dl.start_timer(s.seq);
     }
 
     return 0;
