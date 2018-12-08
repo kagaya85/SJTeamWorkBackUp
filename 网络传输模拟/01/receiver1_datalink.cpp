9d8ebc81cde45137f9a9a70bac8f775a2773c222
Kagaya <kagaya85@foxmail.com>
2018-12-09 02:21:39 +0800

    update

--- 网络传输模拟/01/receiver1_datalink.cpp
+++ 网络传输模拟/01/receiver1_datalink.cpp
@@ -19,10 +19,14 @@ int main()
         pid = getPidByName("physical");
     }
 
+
     while(true)
     {
+        cout << "datalink start wait" << endl;
         dl.wait_for_event(&event);
+        cout << "datalink read from physical" << endl;
         dl.from_physical_layer(&r);
+        cout << "datalink write to network" << endl;
         dl.to_network_layer(&r.info);
     }
 
